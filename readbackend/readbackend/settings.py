975032deb01cd703c2c610be325f2929b1fb33af
Yramklass <yramklass@gmail.com>
2024-08-05 11:44:42 +0200

    Added DjangoRestFramwework and PostgreSQL dependencies to the project, giving it REST Api functionality and database functionality.

--- readbackend/readbackend/settings.py
+++ readbackend/readbackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.core',
     'apps.users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'readbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users',
+        'USER': 'postgres',
+        'PASSWORD': 'Kenzo*777',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
