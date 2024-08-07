d668e75db8c22d10f2e3c61763f86152473e8aa8
Yramklass <yramklass@gmail.com>
2024-08-07 20:48:48 +0200

    Testing Branch Merging

--- readbackend/apps/users/views.py
+++ readbackend/apps/users/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from models import Users
+from faster_whisper import WhisperModel
 
 # Create your views here.
+
+# Model reccomended by the Client
+model_size = "large-v3"
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+
