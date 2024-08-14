060fbff611c463474425a573cf230f8c80caadcd
Yashveer Ramklass <rmkyas002@cs.uct.ac.za>
2024-08-14 21:48:22 +0200

    Revert "added the voice and test analysis, just need to debug a permission error"
    This reverts commit 177fb1360b13f88f174e00f022cd2443d847b5c1

--- readbackend/readbackend/settings.py
+++ readbackend/readbackend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from decouple import config
 from pathlib import Path
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'readbackend.readbackend.settings')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,10 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'readbackend.apps.users',
-    
-   
-
+    'apps.core',
+    'apps.users',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
